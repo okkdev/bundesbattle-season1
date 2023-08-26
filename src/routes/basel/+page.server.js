1ff5c555b6f595969526e93b5c854a046e13465b
okkdev <dev@stehlik.me>
2023-08-26 23:29:58 +0200

    add reset logo

--- src/routes/basel/+page.server.js
+++ src/routes/basel/+page.server.js
@@ -1,4 +1,3 @@
-import { error } from "@sveltejs/kit"
 import { fetchRange } from "$lib/sheetsAPI"
 
 /** @type {import('./$types').PageServerLoad} */
--- src/routes/direct-qualifiers/+page.svelte
+++ src/routes/direct-qualifiers/+page.svelte
@@ -4,24 +4,24 @@
   console.log(data)
 </script>
 
-<div class="grid grid-cols-1 gap-10 sm:grid-cols-2 max-w-4xl mx-auto">
-  <div class="flex flex-col justify-center items-center gap-2">
+<div class="grid max-w-4xl grid-cols-1 gap-10 mx-auto sm:grid-cols-2">
+  <div class="flex flex-col items-center gap-2">
     <img
-      src="images/thereset-logo.png"
+      src="images/thereset-logo.svg"
       alt="TheReset #8 Logo"
-      class="object-contain h-80 w-auto"
+      class="object-contain w-auto h-80 aspect-square"
     />
-    <h1 class="font-stencil uppercase text-2xl">TheReset #8</h1>
+    <h1 class="text-2xl uppercase font-stencil">TheReset #8</h1>
     <div>Jul 28th — 30th, 2023</div>
     {#if data.thereset}
-      <div class="py-3 text-lg flex gap-2 items-center justify-center">
+      <div class="flex items-center justify-center gap-2 py-3 text-lg">
         Winner: <span class="font-semibold">{data.thereset[0][0]}</span>
         {#if data.thereset[0][1]}
           <img
             src="/images/wappen/{data.thereset[0][1]}.svg"
             alt={data.thereset[0][1]}
             title={data.thereset[0][1]}
-            class="h-5 w-auto"
+            class="w-auto h-5"
           />
         {/if}
       </div>
@@ -29,34 +29,34 @@
 
     <a
       href="https://www.start.gg/tournament/thereset-8/details"
-      class="py-2 px-3 rounded bg-bbyellow text-slate-700 hover:bg-yellow-200"
+      class="px-3 py-2 rounded bg-bbyellow text-slate-700 hover:bg-yellow-200"
       >start.gg</a
     >
   </div>
-  <div class="flex flex-col justify-center items-center gap-2">
+  <div class="flex flex-col items-center gap-2">
     <img
       src="images/gaminghotel-logo.png"
       alt="GamingHotel2023 Logo"
-      class="object-contain h-80 w-auto"
+      class="object-contain w-auto h-80 aspect-square"
     />
-    <h1 class="font-stencil uppercase text-2xl">GamingHotel 2023</h1>
+    <h1 class="text-2xl uppercase font-stencil">GamingHotel 2023</h1>
     <div>Sep 9th — 10th, 2023</div>
     {#if data.gaminghotel}
-      <div class="py-3 text-lg flex gap-2 items-center justify-center">
+      <div class="flex items-center justify-center gap-2 py-3 text-lg">
         Winner: <span class="font-semibold">{data.gaminghotel[0][0]}</span>
         {#if data.gaminghotel[0][1]}
           <img
             src="/images/wappen/{data.gaminghotel[0][1]}.svg"
             alt={data.gaminghotel[0][1]}
             title={data.gaminghotel[0][1]}
-            class="h-5 w-auto"
+            class="w-auto h-5"
           />
         {/if}
       </div>
     {/if}
     <a
       href="https://www.start.gg/tournament/gaminghotel-2023/details"
-      class="py-2 px-3 rounded bg-bbyellow text-slate-700 hover:bg-yellow-200"
+      class="px-3 py-2 rounded bg-bbyellow text-slate-700 hover:bg-yellow-200"
       >start.gg</a
     >
   </div>
