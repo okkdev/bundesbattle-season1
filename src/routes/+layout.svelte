31432a1c80653ed6c6fb44d5750d703e6ba3e8a8
okkdev <dev@stehlik.me>
2023-08-28 15:38:08 +0200

    refactor meta tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,42 +3,33 @@
   import { page } from "$app/stores"
   import { clickOutside } from "$lib/clickOutside.js"
 
+  const title = "BundesBattle Season 1"
+  const description =
+    "The Swiss Street Fighter 6 league with offline qualifiers in Basel and Zürich!"
+  const url = "https://bundesbattle.ch/"
+  const banner = url + "images/bundesbattle-banner.png"
+
   let mobileMenuOpen = false
 </script>
 
 <svelte:head>
-  <title>BundesBattle Season 1</title>
-  <meta name="title" content="BundesBattle Season 1" />
-  <meta
-    name="description"
-    content="The Swiss Street Fighter 6 league with offline qualifiers in Basel and Zürich!"
-  />
+  <title>{title}</title>
+  <meta name="title" content={title} />
+  <meta name="description" content={description} />
 
   <!-- Open Graph / Facebook -->
   <meta property="og:type" content="website" />
-  <meta property="og:url" content="https://bundesbattle.ch/" />
-  <meta property="og:title" content="BundesBattle Season 1" />
-  <meta
-    property="og:description"
-    content="The Swiss Street Fighter 6 league with offline qualifiers in Basel and Zürich!"
-  />
-  <meta
-    property="og:image"
-    content="https://bundesbattle.ch/images/bundesbattle-banner.png"
-  />
+  <meta property="og:url" content={url} />
+  <meta property="og:title" content={title} />
+  <meta property="og:description" content={description} />
+  <meta property="og:image" content={banner} />
 
   <!-- Twitter -->
   <meta property="twitter:card" content="summary_large_image" />
-  <meta property="twitter:url" content="https://bundesbattle.ch/" />
-  <meta property="twitter:title" content="BundesBattle Season 1" />
-  <meta
-    property="twitter:description"
-    content="The Swiss Street Fighter 6 league with offline qualifiers in Basel and Zürich!"
-  />
-  <meta
-    property="twitter:image"
-    content="https://bundesbattle.ch/images/bundesbattle-banner.png"
-  />
+  <meta property="twitter:url" content={url} />
+  <meta property="twitter:title" content={title} />
+  <meta property="twitter:description" content={description} />
+  <meta property="twitter:image" content={banner} />
 </svelte:head>
 
 <nav
